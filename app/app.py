71d1fecb27a7f78d2a604f2552cdb55a9f2ce0d4
Umair Ali <umair9009@hotmail.com>
2026-08-03 14:19:47 +0100

    Add health check endpoint

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "message": "Secure Cloud Delivery Platform API"
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
